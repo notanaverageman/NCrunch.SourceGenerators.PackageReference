e5736ccc66448b32fb7ecce39b898893b0532c00
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2021-08-19 15:40:36 +0300

    Add project files.

---
+++ CodeGeneration/Generator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace CodeGeneration
+{
+	[Generator]
+	public class Generator : ISourceGenerator
+	{
+		public void Initialize(GeneratorInitializationContext context)
+		{
+		}
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+			string json = "{'Name': 'test'}";
+			JsonConvert.DeserializeObject(json);
+
+			context.AddSource("Test", "class Test {}");
+		}
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ Library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Test test = new Test();
+		}
+	}
+}
